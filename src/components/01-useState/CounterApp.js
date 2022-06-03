86aec235802def18e3bf87381881258570608b64
Esteban Sichacá <estebangee@hotmail.com>
2022-06-03 17:45:59 -0500

    Custom hook counter

--- src/components/01-useState/CounterApp.js
+++ src/components/01-useState/CounterApp.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import './Counter.css'
+import React, { useState } from 'react';
+import './Counter.css';
 
 export const CounterApp = () => {
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HookApp } from './HookApp';
-import { CounterApp } from './components/01-useState/CounterApp';
+// import { HookApp } from './HookApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustimHook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <CounterApp />
+  <CounterWithCustomHook />
 );
 
 
