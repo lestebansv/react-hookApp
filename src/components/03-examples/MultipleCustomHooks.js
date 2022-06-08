180bfbfbc0d863f29b7dce4a1d8de093050e3c5c
Esteban Sichacá <estebangee@hotmail.com>
2022-06-07 21:36:10 -0500

    custom hook useFetch

---
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import { useCounter } from '../../hooks/useCounter';
+import '../02-useEffect/effects.css';
+
+export const MultipleCustomHooks = () => {
+
+    const { counter, increment } = useCounter(1);
+
+    const { loading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+
+    const { author, quote } = !!data && data[0];
+
+    return (
+        <div>
+            <h1>Breakig Bad Quotes</h1>
+            <hr />
+
+            {
+                loading
+                    ?
+                    (
+                        <div className='alert alert-info text-center'>
+                            Loading...
+                        </div>
+                    )
+                    :
+                    <blockquote className='blockquote' style={{ textAlign: "right" }}>
+                        <p className='mb-0'> {quote} </p>
+
+                        <br />
+                        <footer className='blockquote-footer'>{author}</footer>
+                    </blockquote>
+            }
+
+            <button className='btn btn-primary' onClick={increment}>
+                Siguente quote
+            </button>
+
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom/client';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustimHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <FormWithCustomHook />
+  <MultipleCustomHooks />
 );
 
 
