b27a9a3a1d7947fb83f950cc546254b86f4c327f
Esteban Sichacá <estebangee@hotmail.com>
2022-06-08 22:27:02 -0500

    useMemo and useCallback

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ import ReactDOM from 'react-dom/client';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 // import { Layout } from './components/05-useLayoutEffect/Layout';
-import { Memorize } from './components/06-memos/Memorize';
+// import { Memorize } from './components/06-memos/Memorize';
+// import { MemoHook } from './components/06-memos/MemoHook';
+import { CallbackHook } from './components/06-memos/CallbackHook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Memorize />
+  <CallbackHook />
 );
 
 
