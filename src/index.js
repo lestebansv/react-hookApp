61c2f133cca5b2f0c6f6de35559d37fdbe69eab2
Esteban Sichacá <estebangee@hotmail.com>
2022-06-08 21:00:22 -0500

    Memo - método de react

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import ReactDOM from 'react-dom/client';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-import { Layout } from './components/05-useLayoutEffect/Layout';
+// import { Layout } from './components/05-useLayoutEffect/Layout';
+import { Memorize } from './components/06-memos/Memorize';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Layout />
+  <Memorize />
 );
 
 
